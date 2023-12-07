d3472a5ab3e25c5f018915e6f4ae0dff95d6d5f5
danielabib04 <danielabib@outlook.com>
2023-12-06 21:37:45 -0300

    adiciona index.html, estoque.js e main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+//main.js
+
+
+
+
+document.entrada.addEventListener('submit', leFormulario)
+
+const olJoao = document.querySelector('#olJoao');
+const olMaria = document.querySelector('#olMaria');
+function leFormulario(event){
+    event.preventDefault();
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.quantidade.value;
+
+    console.log(`fruta: ${fruta} qtd: ${quantidade}`);
+    document.entrada.submit();
+};
